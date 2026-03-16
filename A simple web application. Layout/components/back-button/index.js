f98b917e4887f30a6f470f335415a1b9e9a2efb1
stepaperfil <stepaperfyt@yandex.ru>
2026-03-16 13:19:45 +0300

    Add files via upload

--- A simple web application. Layout/components/back-button/index.js
+++ A simple web application. Layout/components/back-button/index.js
@@ -1,1 +1,24 @@
+export class BackButtonComponent {
+    constructor(parent) {
+        this.parent = parent;
+    }
 
+    addListeners(listener) {
+        document.getElementById("back-button").addEventListener("click", listener);
+    }
+
+    getHTML() {
+        return `
+            <button id="back-button" class="btn btn-secondary"
+                    style="background: #7545E0; border: none; border-radius: 16px; margin-bottom: 20px;">
+                ← Назад
+            </button>
+        `;
+    }
+
+    render(listener) {
+        const html = this.getHTML();
+        this.parent.insertAdjacentHTML('beforeend', html);
+        this.addListeners(listener);
+    }
+}
